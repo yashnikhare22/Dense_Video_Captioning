5be3c3a27308525e331c3b626dd97e25769a74b4
Yash Nikhare <49709163+yashnikhare22@users.noreply.github.com>
2023-04-27 11:03:50 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+train_path = "/content/drive/MyDrive/Dense-Video-Captioning/data/training_data"
+test_path = "/content/drive/MyDrive/Dense-Video-Captioning/data/testing_data/"
+batch_size = 160#80#320
+learning_rate = 0.0007
+epochs = 150
+latent_dim =256
+num_encoder_tokens = 2560
+num_decoder_tokens = 1500
+time_steps_encoder = 20
+max_probability = -1
+save_model_path = '/content/drive/MyDrive/Dense-Video-Captioning/model_final_2'
+validation_split = 0.15
+max_length = 10
+search_type = 'greedy'
